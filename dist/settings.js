194e3da0c2ba61a3234f8e51d1f9006fb7e7d284
JayChase <JonathanChase@outlook.com>
2015-09-08 17:06:06 +0900

    added build tasks

---
+++ dist/settings.js
@@ -0,0 +1,32 @@
+//Note the file functions here are synchronous as require is synchronous anyway. 
+function settings(fs, customSettings, settingsFiles) {
+	var _ = require('lodash'),
+		settings = {
+		};		
+
+	settingsFiles = _.isArray(settingsFiles) ? settingsFiles : ['./config.json', './secrets.json'];
+	customSettings = _.isObject(customSettings) ? customSettings : {};
+	
+	settingsFiles.forEach(function(settingsFile){
+		_.assign(settings,loadFile(settingsFile));
+	});
+
+	_.assign(settings, customSettings);
+
+	return settings;
+
+	function loadFile(filePath) {
+		if (fs.existsSync(filePath)) {
+			var file = fs.readFileSync(filePath, 'utf8')
+			try {
+				return JSON.parse(file);
+			} catch (error) {
+				return {};
+			}
+		} else {
+			return {};
+		}
+	}
+}
+
+module.exports = settings;
--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp'),
+	del = require('del'),
 	gldr = require('gulp-load-plugins')({ lazy: true });
 
 gulp.task('test', function (callback) {
@@ -10,3 +11,24 @@ gulp.task('test', function (callback) {
 gulp.task('watch', function (callback) {	
 	gulp.watch(['./**/*.js','test/**/*.js'],['test']);
 });
+
+gulp.task('build', ['copy-source-to-build'], function () {
+
+});
+
+gulp.task('copy-source-to-build', ['minify'], function () {
+	return gulp.src('./source/*.js')
+		.pipe(gulp.dest('dist'));
+});
+
+gulp.task('minify', ['clean-dist'], function () {
+	return gulp.src('./source/*.js')
+		.pipe(gldr.uglify())
+		.pipe(gldr.rename({ suffix: '.min' }))
+		.pipe(gulp.dest('dist'));
+});
+
+gulp.task('clean-dist', ['test'], function () {
+	del('dist/**.*');
+});
+
